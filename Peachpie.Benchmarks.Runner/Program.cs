d0fb76c0bd5d68563c5ed710eca151a04a3fbbec
Robert Husák <robert@husak.cloud>
2020-01-11 19:17:02 +0100

    Implement basic benchmarking mechanism

---
+++ Peachpie.Benchmarks.Runner/Program.cs
@@ -0,0 +1,59 @@
+﻿extern alias transformations_Debug;
+using TransformationsDebug = transformations_Debug::Peachpie.Benchmarks.Transformations;
+using TransformationsRelease = Peachpie.Benchmarks.Transformations;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Parameters;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Exporters;
+
+namespace Peachpie.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize common settings
+            var job = Job.ShortRun
+                .With(InProcessEmitToolchain.Instance)  // The current .NET Core toolchain requires the DLLs to be named exactly as their projects
+                .WithBaseline(true);
+            var parameters = new ParameterInstances(Array.Empty<ParameterInstance>());
+            var config = DefaultConfig.Instance
+                .With(ConfigOptions.DisableOptimizationsValidator)
+                .With(MemoryDiagnoser.Default)
+                .With(TargetMethodColumn.Type)
+                .With(BenchmarkLogicalGroupRule.ByCategory)
+                .CreateImmutableConfig();
+
+            var debugAssembly = typeof(TransformationsDebug.Helper).Assembly;
+            var releaseAssembly = typeof(TransformationsRelease.Helper).Assembly;
+
+            // Add a method from each class in both Debug and Release version
+            var cases = new List<BenchmarkCase>();
+            foreach (var debugType in debugAssembly.GetTypes())
+            {
+                var debugMethod = debugType.GetMethod("run");
+                if (debugMethod == null)
+                    continue;
+
+                var releaseType = releaseAssembly.GetType(debugType.FullName);
+                var releaseMethod = releaseType.GetMethod("run");
+
+                cases.Add(BenchmarkCase.Create(new Descriptor(debugType, debugMethod), job, parameters, config));
+                cases.Add(BenchmarkCase.Create(new Descriptor(releaseType, releaseMethod), job, parameters, config));
+            }
+
+            var runInfo = new BenchmarkRunInfo(cases.ToArray(), typeof(TransformationsDebug.Helper), config);
+            BenchmarkRunner.Run(runInfo);
+        }
+    }
+}
---
+++ Peachpie.Benchmarks/transformations.php
@@ -0,0 +1,24 @@
+﻿<?php
+namespace Peachpie\Benchmarks\Transformations;
+
+class Helper {}
+
+class Ord {
+    private final function ord_string_int(string $s, int $i) {
+      return ord($s[$i]);
+    }
+
+    public final function run() {
+        return $this->ord_string_int("foo", 1);
+    }
+}
+
+class CallableThis {
+    private function bar() {
+        return 42;
+    }
+
+    public final function run() {
+        return call_user_func([$this, "bar"]);
+    }
+}
