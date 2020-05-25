6143f462c086949f76c07bd8db9486dcc390bad8
Jakub Míšek <jmisek@gmail.com>
2020-05-25 12:36:58 +0200

    call_user_func test, ignored unreachability warning

--- microbenchmarks/functions/call_user_func.php
+++ microbenchmarks/functions/call_user_func.php
@@ -2,6 +2,14 @@
 
 function _bar() {}
 
-function call_user_func_test() {
+function call_user_func_string() {
     call_user_func(__NAMESPACE__ ."\\_bar");
+}
+
+class CallableObject {
+    function __invoke() { }
+}
+
+function call_user_func_object() {
+    call_user_func(new CallableObject);
 }
